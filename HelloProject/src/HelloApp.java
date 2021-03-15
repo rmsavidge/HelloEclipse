c6b6e9e0cd2e557ff58e2b595dc24096ca22487b
Rachel Savidge <rachel.savidge@gmail.com>
2021-03-15 10:38:26 -0300

    version 3

--- HelloProject/src/HelloApp.java
+++ HelloProject/src/HelloApp.java
@@ -3,7 +3,8 @@ public class HelloApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Hello Rachel Savidge!!");
+		System.out.println("Hello Rachel Savidge!!");
+		System.out.println("Just checking this again.");
 	}
 
 }
